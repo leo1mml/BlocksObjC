1c22047a861c5682ae71df900203c6e52722bce9
Leonel Menezes <leonelmenezes@mac325.local>
2017-03-27 15:40:43 -0300

    Initial Commit

---
+++ BlocksObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BlocksObjC
+//
+//  Created by Leonel Menezes on 27/03/17.
+//  Copyright © 2017 BEPID. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
